3cdcc95763e844b3bcc3b44f7c5a929ed1faa2e8
Moses B <thedevmosesb@gmail.com>
2024-08-02 15:44:42 +0000

    Pending changes exported from your codespace

--- backend.py
+++ backend.py
@@ -12,39 +12,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.service import Service
 
 class Browser(object):
-	def initialize_browser(self, initialize_attempts=0):
-		initialize_attempts += 1
-		try:
-			# Initialize chrome_options
-			chrome_options = uc.ChromeOptions()
-			# Initialize the Chrome webdriver
-			print(f"LOADING: Opening Chrome, This may take a while.")
-			# Patch to fix a bug in uc
-			driver = uc.Chrome(options=chrome_options)
-			return driver
-		except:
-			# retry initialize 5 times
-			if initialize_attempts >= 5:
-				print(f"An ERROR OCCURED: While Opening Chrome, Retrying {initialize_attempts}.")
-				#self.call_back.append({traceback.format_exc()})
-				raise
-			else:
-				print(traceback.format_exc())
-				return self.initialize_browser(initialize_attempts)
-	def write_file(self, participants):
-		"""Write attendance data to the disk as a CSV file"""
-		try:
-			with open("attendance.csv", 'w', encoding='utf-8') as f:
-				fields = ['Name']
-				csvwriter = csv.DictWriter(f, fieldnames=fields)
-				for userData in participants:
-					csvwriter.writerow(userData)
-				print("Data saved successfully!")
-		except IOError:
-			print("Error occured while writing to the disk")
-
+	def initialize_browser(self):
+		# Initialize chrome_options
+		chrome_options = uc.ChromeOptions()
+		chrome_options.add_argument("--disable-search-engine-choice-screen")
+		chrome_options.add_argument("--no-sandbox")
+		chrome_options.add_argument('--disable-dev-shm-usage')
+		chrome_options.add_argument("--disable-setuid-sandbox")
+		chrome_options.add_argument("--use-fake-ui-for-media-stream")
+		# Initialize the Chrome webdriver
+		print(f"LOADING: Opening Chrome, This may take a while.")
+		# Patch to fix a bug in uc
+		executable_path = "/usr/local/bin/chromedriver"
+		browser_path = "/usr/local/bin/chrome"
+		driver = uc.Chrome(options=chrome_options, driver_executable_path=executable_path, browser_executable_path=browser_path, patcher_force_close=True)
+		return driver
+		
 	
 
 
@@ -55,6 +41,7 @@ class ZoomBot(object):
 		self.meeting_url = meeting_url
 		self.stop_incoming_video = stop_incoming_video
 		self.participants = []
+		self.js_code =""
 	def join_meeting(self, join_attempts=0):
 		join_attempts += 1
 		try:
@@ -111,49 +98,38 @@ class ZoomBot(object):
 				print("This may be because the meeting has not yet started or has already ended.")
 				return self.join_meeting(join_attempts)
 
-	def log_participants(self, get_attempts=0):
-		"""Log participants' names"""
+	def is_meeting_ongoing(self, get_attempts=0):
+		"""Check if meeting is ongoing and Log new participants' names"""
+		get_attempts+=1
 		try:
-			self.driver.find_elements(By.XPATH, "//span[text()='Participants']")[0].click()
-
+			try:
+			    WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.XPATH,  "//span[text()='Participants']")))
+			    self.driver.find_elements(By.XPATH, "//span[text()='Participants']")[0].click()
+			except:
+			    pass
 			users = self.driver.find_elements(By.CLASS_NAME, "participants-item__name-section")
+			if len(users[1:]) < 2 :
+			    print("Participants are less than 2, so meeting has ended. Hurray, Hurray.")
+			    return False
 			for user in users[1:]:
 				tempText = re.findall(r"^<span .*\">(.*)<\/span.*\">(.*)<\/span>", user.get_attribute("innerHTML").strip())
 				uName = tempText[0][0] + tempText[0][1]
 				if not uName in self.participants:
 					self.participants.append(uName)
-			print(f"Searched for meeting participants: {self.participants}")
+					print(f"New meeting participant found: {uName}")
+			return True
 		except Exception as e:
 			# retry initialize 5 times
 			if get_attempts >= 5:
 				print(e)
 				print('Did not find meeting participants')
-			else:
-				print(f"An ERROR OCCURED: While Trying To get Zoom Meeting participants, Retrying {get_attempts}.")
-				return self.log_participants(get_attempts)
-			
-	def get_current_speaker(self, get_attempts=0):
-		try:
-			active_speaker_container = self.driver.find_element(By.CLASS_NAME, 'speaker-active-container__wrap')
-			video_frame = active_speaker_container.find_element(By.CLASS_NAME, 'speaker-active-container__video-frame')
-			try:
-				current_speaker_name = video_frame.find_element(By.CLASS_NAME, 'video-avatar__avatar-name').text
-			except:
-				current_speaker_name = video_frame.find_element(By.CLASS_NAME, 'video-avatar__avatar-img').get_attribute('alt')
-			if not current_speaker_name in self.participants:
-				self.log_participants()
-			return current_speaker_name
-		except Exception as e:
-			# retry initialize 5 times
-			if get_attempts >= 5:
-				print(e)
-				print('Did not find the current speaker in the meeting')
 				return None
 			else:
-				print(f"An ERROR OCCURED: While Trying To get the current speaker in the Zoom Meeting, Retrying {get_attempts}.")
-				return self.get_current_speaker(get_attempts)
+				print(f"An ERROR OCCURED: While Trying To get Zoom Meeting participants, Retrying {get_attempts}.")
+				return self.is_meeting_ongoing(get_attempts)
 
-	
+
+
 
 
 class GoogleMeetBot(object):
@@ -163,15 +139,46 @@ class GoogleMeetBot(object):
 		self.bot_name = bot_name
 		self.participant_number = 0
 		self.participants = []
+		self.js_code =""
+		self.google_username="pythonrobot007@gmail.com"
+		self.google_password="python&007"
+	def login(self, login_attempts=0): 
+		login_attempts += 1
+		url = 'https://accounts.google.com/'
+		try:
+			# google login page
+			self.driver.get(url)
+			# Finding username textbox and logging the username
+			WebDriverWait(self.driver, 30).until, EC.presence_of_element_located((By.ID, 'identifierId')).send_keys(self.google_username)
+			WebDriverWait(self.driver, 30).until, EC.presence_of_element_located((By.ID, 'identifierNext')).click()		
+			# Finding password textbox and logging the password
+			WebDriverWait(self.driver, 30).until, EC.presence_of_element_located((By.NAME, 'password')).send_keys(self.google_password)
+			WebDriverWait(self.driver, 30).until, EC.presence_of_element_located((By.ID, 'passwordNext')).click()
+			print("Logging Into Google")
+			# HERE - WebDriverWait for login to complete successfully
+		except:
+			# retry initialize 5 times
+			if login_attempts >= 5:
+				print(traceback.format_exc())
+				self.driver.close()
+				raise
+			else:
+				print(f"An ERROR OCCURED: While Trying To Login to Google Account, Retrying {login_attempts}.")
+				print("This may be because of 2-Factor Authentication.")
+				return self.login(login_attempts)
+				
 	def join_meeting(self, join_attempts=0):
 		join_attempts += 1
 		try:
 			self.driver.get(self.meeting_url)
-			# Wait until the element is visible and clickable
-			continue_button_xpath = "//span[contains(text(), 'Continue without microphone')]"
-			continue_button = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, continue_button_xpath)))
-			# Click the element
-			continue_button.click()
+			try:
+				# Wait until the element is visible and clickable
+				continue_button_xpath = "//span[contains(text(), 'Continue without microphone')]"
+				continue_button = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, continue_button_xpath)))
+				# Click the element
+				continue_button.click()
+			except:
+				pass
 			print(f"LOADING: {self.meeting_url} - {join_attempts} attempts")
 			# Wait until the input field is visible
 			name_input_xpath = "//input[@placeholder='Your name']"
@@ -194,47 +201,34 @@ class GoogleMeetBot(object):
 				print(f"An ERROR OCCURED: While Trying To Join Google Meet Meeting, Retrying {join_attempts}.")
 				print("This may be because the meeting has not yet started or has already ended.")
 				return self.join_meeting(join_attempts)
-	
-	def log_participants(self, get_attempts=0):
-		"""Log participants' names"""
+				
+	def is_meeting_ongoing(self, get_attempts=0):
+		get_attempts+=1
+		"""Check if meeting is ongoing and Log new participants' names"""
 		try:
+			WebDriverWait(self.driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-self-name='You']")))
 			participant_elements = self.driver.find_elements(By.CSS_SELECTOR, "[data-self-name='You']")
+			if len(participant_elements) < 2 :
+			    print("Participants are less than 2, so meeting has ended. Hurray, Hurray.")
+			    return False
 			for element in participant_elements:
 				# Find the actual participant name within the element
 				name_element = element.find_element(By.CLASS_NAME, 'dwSJ2e')
-				self.participants.append(name_element.text)
-			print(f"Searched for meeting participants: {self.participants}")
+				uName = name_element.text
+				if not uName in self.participants:
+				    self.participants.append(uName)
+				    print(f"New meeting participant found: {uName}")
+			return True
 		except Exception as e:
 			# retry initialize 5 times
 			if get_attempts >= 5:
 				print(e)
-				print('Did not find meeting participants')
+				print('Error: is_meeting_ongoing: Did not find meeting participants')
+				return None
 			else:
-				print(f"An ERROR OCCURED: While Trying To get Google Meeting participants, Retrying {get_attempts}.")
-				return self.log_participants(get_attempts)
+				print(f"An ERROR OCCURED: While Trying To log Google Meeting participants, Retrying {get_attempts}.")
+				return self.is_meeting_ongoing(get_attempts)
 	
-	def get_current_speaker(self, get_attempts=0):
-		"""Get participants' names"""
-		try:
-			if self.participant_number != len(self.participants):
-				participant_elements = self.driver.find_elements(By.CSS_SELECTOR, "[data-self-name='You']")
-				for element in participant_elements:
-					# Find the actual participant name within the element
-					name_element = element.find_element(By.CLASS_NAME, 'dwSJ2e')
-					if not name_element.text in self.participants:
-						self.participants.append(name_element.text)
-				print(f"Searched for meeting participants: {self.participants}")
-				return str(self.participants)
-			else:
-				return ""
-		except Exception as e:
-			# retry initialize 5 times
-			if get_attempts >= 5:
-				print(e)
-				print('Did not find meeting participants')
-			else:
-				print(f"An ERROR OCCURED: While Trying To get Google Meet participants, Retrying {get_attempts}.")
-				return self.get_current_speaker(get_attempts)
 	
 	
 
@@ -250,6 +244,40 @@ class MicrosoftTeamsBot(object):
         self.meeting_url = meeting_url
         self.bot_name = bot_name
         self.participants = []
+        self.js_code =""
+        self.microsoft_username="pythonrobot007@gmail.com"
+        self.microsoft_password="python&007"
+    def login(self, login_attempts=0): 
+        login_attempts += 1
+        try:
+            # go to microsoft page
+            self.driver.get(self.meeting_url)
+            # wait for page to load
+            WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.TAG_NAME,'body')))
+            if("login.microsoftonline.com" in self.driver.current_url):
+                # Finding username textbox and logging the username
+                emailField = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, 'i0116')))
+                emailField.click()
+                emailField.send_keys(self.microsoft_username)
+                WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, 'idSIButton9'))).click() #Next button
+                # Wait for the password page to load and input password
+                passwordField = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, 'i0118')))
+                passwordField.click()
+                passwordField.send_keys(self.microsoft_password)
+                WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, 'idSIButton9'))).click()
+                # Handle stay signed in prompt
+                WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.ID, 'idSIButton9'))).click() #remember login
+                print("Logging Into microsoft")
+        except:
+            # retry initialize 5 times
+            if login_attempts >= 5:
+                print(traceback.format_exc())
+                self.driver.close()
+                raise
+            else:
+                print(f"An ERROR OCCURED: While Trying To Login To Microsoft Account, Retrying {login_attempts}.")
+                print("This may be because of 2-Factor Authentication.")
+                return self.login(login_attempts)
 
     def join_meeting(self, join_attempts=0):
         join_attempts += 1
@@ -301,12 +329,9 @@ class MicrosoftTeamsBot(object):
                 print("This may be because the meeting has not yet started or has already ended.")
                 return self.join_meeting(join_attempts)
 
-    def log_participants(self, get_attempts=0):
-        pass
-    def get_current_speaker(self, get_attempts=0):
-        return ""
-
-
+    def is_meeting_ongoing(self, get_attempts=0):
+        print("INFO: On Microsoft Teams, by default Selenium Bot uses JavaScript code to check if meeting has ended.")
+        return None
 
 
 
